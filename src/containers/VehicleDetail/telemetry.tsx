cce8854da36d751c8d13e32999025985a1fcae93
Naci Dai <naci.dai@eteration.com>
2022-10-21 10:30:16 +0300

    Refactored telemetry form

--- src/containers/VehicleDetail/telemetry.tsx
+++ src/containers/VehicleDetail/telemetry.tsx
@@ -37,7 +37,8 @@ import {
   Button,
   Grid,
   GridItem,
-  Gallery
+  Gallery,
+  Spinner
 } from '@patternfly/react-core'
 import HelpIcon from '@patternfly/react-icons/dist/esm/icons/help-icon'
 
@@ -48,70 +49,175 @@ import { filterList } from '../../common/filter'
 import { GETTHINGS } from '../../api/query/vehicle'
 import { useLazyQuery } from '@apollo/client'
 
+const TelemetryForm = ({ client, definition, onSubmit, loading }) => {
+  const [rate, setRate] = useState(definition?.rate)
+  const [target, setTarget] = useState(definition?.target?.topic)
+
+  const handleRateChange = (trate: string, _event) => {
+    setRate(parseInt(trate))
+  }
+  const handleTargetChange = (name: string, _event) => {
+    setTarget(name)
+  }
+
+  const addTelemetery = () => {
+    if (definition) {
+      definition.rate = rate
+      definition.target.topic = target
+      onSubmit(client, definition, 'register')
+    }
+  }
+  return definition
+    ? (!loading
+        ? (
+            <Card style={{ textAlign: 'left', margin: '10px' }} component="div"><CardBody><Form>
+            <FormGroup label="Topic name" fieldId="simple-form-note-01">
+              <TextInput isDisabled type="text" id="simple-form-note-01" name="simple-form-number" value={definition?.topic} />
+            </FormGroup>
+            <FormGroup
+              label="Rate(ms)"
+              labelIcon={
+                <Popover
+                  headerContent={
+                    <div>
+                      The number of milliseconds between each message published. (i.e. 1000ms for a second)
+                    </div>
+                  }
+                  bodyContent={
+                    <div>
+                      Milli seconds between each message.
+                    </div>
+                  }
+                >
+                  <button
+                    type="button"
+                    aria-label="More info for name field"
+                    onClick={e => e.preventDefault()}
+                    aria-describedby="simple-form-name-01"
+                    className="pf-c-form__group-label-help"
+                  >
+                    <HelpIcon noVerticalAlign />
+                  </button>
+                </Popover>
+              }
+              isRequired
+              fieldId="simple-form-name-01"
+              helperText="Milliseconds between each message."
+            >
+              <TextInput
+                isRequired
+                type="number"
+                id="simple-form-name-01"
+                name="simple-form-name-01"
+                aria-describedby="simple-form-name-01-helper"
+                value={rate}
+                onChange={handleRateChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label="Target mqtt channnel"
+              labelIcon={
+                <Popover
+                  headerContent={
+                    <div>
+                      The name of the mqtt channel where this information will be published
+                    </div>
+                  }
+                  bodyContent={
+                    <div>
+                      Typically starts with the thingId/telemetry/ followed by a unique path.
+                    </div>
+                  }
+                >
+                  <button
+                    type="button"
+                    aria-label="More info for name field"
+                    onClick={e => e.preventDefault()}
+                    aria-describedby="simple-form-name-01"
+                    className="pf-c-form__group-label-help"
+                  >
+                    <HelpIcon noVerticalAlign />
+                  </button>
+                </Popover>
+              }
+              isRequired
+              fieldId="simple-form-name-01"
+              helperText="Typically starts with the thingId/telemetry/ followed by a unique path identifying the topic and other selectors e.g: 'ai.composiv.sandbox.f1tenth:mycar/telemetry/atopic/abcd-1234-efgh'."
+            >
+              <TextInput
+                isRequired
+                type="text"
+                id="simple-form-name-01"
+                name="simple-form-name-01"
+                aria-describedby="simple-form-name-01-helper"
+                value={target}
+                onChange={handleTargetChange}
+              />
+            </FormGroup>
+            <ActionGroup>
+              <Button variant="primary" onClick={_e => addTelemetery()}>Add Telemetry</Button>
+            </ActionGroup>
+          </Form></CardBody>
+  </Card>)
+        : <Spinner isSVG size="xl" aria-label="Adding telemetry" />)
+    : null
+}
+
 const VehicleTelemetry = () => {
-  const [myuuid] = useState(uuidv4())
-  const [connectionStatus, setConnectionStatus] = useState(false)
   const location = useLocation()
 
-  const [rate, setRate] = useState(location.state?.definition?.rate)
-  const [target, setTarget] = useState(location.state?.definition?.target?.topic)
-
   const [vehicle, setVehicle] = useState(location.state?.vehicle)
-  const [client, setClient] = useState<any>()
   const [newDefinition, setNewDefinition] = useState(location.state?.definition)
+  const [telemetryList, setTelemetryList] = useState(vehicle?.features?.telemetry?.properties?.definition)
+
+  const [telemetryData, setTelemetryData] = useState({})
+  const [filterValue, setFilterValue] = useState('')
+  const [expanded, setExpanded] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const [myuuid] = useState(uuidv4())
+  const [connectionStatus, setConnectionStatus] = useState(false)
+  const [client, setClient] = useState<any>()
   const [targetTopic] = useState(`db-${vehicle.thingId}/agent/${myuuid}`)
   const [getStacksWithIdLike] = useLazyQuery(GETTHINGS, {
     fetchPolicy: 'no-cache'
   })
 
-  const [telemetryList, setTelemetryList] = useState(vehicle?.features?.telemetry?.properties?.definition)
-  const [telemetryData, setTelemetryData] = useState({})
+  const galleryStyle: any = { '--pf-l-gallery--GridTemplateColumns--min': '260px' }
 
-  const [filterValue, setFilterValue] = useState('')
-
-  const [expanded, setExpanded] = React.useState('')
-
-  const galleryStyle:any = { '--pf-l-gallery--GridTemplateColumns--min': '260px' }
-
-  const showTelemetry = (_topic, payload, _packet) => {
-    for (let index = 0; index < telemetryList?.length; index++) {
-      const t = telemetryList[index]
-      if (t.target.topic === _topic) {
-        console.log(JSON.parse(payload.toString()))
-        telemetryData[_topic] = JSON.parse(payload.toString())
-        setTelemetryData(telemetryData)
-        return true
-      }
+  const onCommandResponse = (_topic, payload, _packet) => {
+    setLoading(false)
+    if (_topic !== targetTopic) return
+    const data: any = JSON.parse(payload.toString())
+    if (data?.status === 'STOPPED' || data?.status === 'DELETED') {
+      setTelemetryData({})
     }
-    return false
+    setLoading(true)
+    getStacksWithIdLike({
+      variables: {
+        filter: `and(eq(thingId,"${vehicle?.thingId}"))`
+      },
+      fetchPolicy: 'no-cache'
+    }).then((result) => {
+      setLoading(false)
+      if (result?.data?.things && result?.data?.things.items.length > 0) {
+        const theVehicle = result.data.things.items[0]
+        const tList = theVehicle.features?.telemetry?.properties?.definition
+        setVehicle(theVehicle)
+        setTelemetryList(tList)
+      }
+    }).finally(() => {
+      setLoading(false)
+    })
   }
+
   useEffect(() => {
     const cl = connect({
       thingId: vehicle.thingId,
       uuid: myuuid,
       onConnect: () => setConnectionStatus(true),
       onFailed: (err) => !!err && setConnectionStatus(false),
-      onMessage: (_topic, payload, _packet) => {
-        if (_topic !== targetTopic) return
-        const data:any = JSON.parse(payload.toString())
-        if (data?.status === 'STOPPED' || data?.status === 'DELETED') {
-          setTelemetryData({})
-        }
-        getStacksWithIdLike({
-          variables: {
-            filter: `and(eq(thingId,"${vehicle?.thingId}"))`
-          },
-          fetchPolicy: 'no-cache'
-        }).then((result) => {
-          if (result?.data?.things && result?.data?.things.items.length > 0) {
-            const theVehicle = result.data.things.items[0]
-            const tList = theVehicle.features?.telemetry?.properties?.definition
-            setVehicle(theVehicle)
-            setTelemetryList(tList)
-            setNewDefinition(undefined)
-          }
-        })
-      }
+      onMessage: onCommandResponse
     })
     setClient(cl)
     doTelemetry(cl, undefined, 'reset')
@@ -138,9 +244,24 @@ const VehicleTelemetry = () => {
       doTelemetry(client, telemetry, 'stop')
     })
   }
-  const doTelemetry = (client, telemetry, action) => {
-    if (telemetry) {
-      client.publish(
+
+  const onTelemetryMessage = (_topic, payload, _packet) => {
+    setLoading(false)
+    for (let index = 0; index < telemetryList?.length; index++) {
+      const t = telemetryList[index]
+      if (t.target.topic === _topic) {
+        console.log(JSON.parse(payload.toString()))
+        telemetryData[_topic] = JSON.parse(payload.toString())
+        setTelemetryData(telemetryData)
+        return true
+      }
+    }
+    return false
+  }
+
+  const doTelemetry = (c, telemetry, action) => {
+    if (c && telemetry) {
+      c.publish(
         `${vehicle.thingId}/agent/commands/ros/topic/echo`,
         JSON.stringify({
           topic: telemetry.topic,
@@ -159,124 +280,23 @@ const VehicleTelemetry = () => {
         }
       )
       if (action === 'start') {
-        client.subscribe(telemetry.target.topic, (err) => {
+        c.subscribe(telemetry.target.topic, (err) => {
           console.log(err)
+          setLoading(false)
         })
-        client.on('message', showTelemetry)
+        c.on('message', onTelemetryMessage)
       }
-    }
-  }
-
-  const handleRateChange = (trate: number) => {
-    setRate(trate)
-  }
-  const handleTargetChange = (name: string) => {
-    setTarget(name)
-    if (newDefinition) { newDefinition.target.topic = name }
-  }
-
-  const addTelemetery = () => {
-    if (newDefinition) {
-      newDefinition.rate = rate
-      newDefinition.target.topic = target
-      doTelemetry(client, newDefinition, 'register')
     }
   }
 
   return (
     <>
       {connectionStatus && <Label color="green" icon={<i className="pf-icon-connected"></i>} >connected to sandbox</Label>}
-      {!!newDefinition && <Card style={{ textAlign: 'left', margin: '10px', padding: '20px' }} component="div" >
-
-        <Form>
-          <FormGroup label="Topic name" fieldId="simple-form-note-01">
-            <TextInput isDisabled type="text" id="simple-form-note-01" name="simple-form-number" value={newDefinition?.topic} />
-          </FormGroup>
-          <FormGroup
-            label="Rate(ms)"
-            labelIcon={
-              <Popover
-                headerContent={
-                  <div>
-                    The number of milliseconds between each message published. (i.e. 1000ms for a second)
-                  </div>
-                }
-                bodyContent={
-                  <div>
-                    Milli seconds between each message.
-                  </div>
-                }
-              >
-                <button
-                  type="button"
-                  aria-label="More info for name field"
-                  onClick={e => e.preventDefault()}
-                  aria-describedby="simple-form-name-01"
-                  className="pf-c-form__group-label-help"
-                >
-                  <HelpIcon noVerticalAlign />
-                </button>
-              </Popover>
-            }
-            isRequired
-            fieldId="simple-form-name-01"
-            helperText="Milliseconds between each message."
-          >
-            <TextInput
-              isRequired
-              type="number"
-              id="simple-form-name-01"
-              name="simple-form-name-01"
-              aria-describedby="simple-form-name-01-helper"
-              value={rate}
-              onChange={handleRateChange}
-            />
-          </FormGroup>
-          <FormGroup
-            label="Target mqtt channnel"
-            labelIcon={
-              <Popover
-                headerContent={
-                  <div>
-                    The name of the mqtt channel where this information will be published
-                  </div>
-                }
-                bodyContent={
-                  <div>
-                    Typically starts with the thingId/telemetry/ followed by a unique path.
-                  </div>
-                }
-              >
-                <button
-                  type="button"
-                  aria-label="More info for name field"
-                  onClick={e => e.preventDefault()}
-                  aria-describedby="simple-form-name-01"
-                  className="pf-c-form__group-label-help"
-                >
-                  <HelpIcon noVerticalAlign />
-                </button>
-              </Popover>
-            }
-            isRequired
-            fieldId="simple-form-name-01"
-            helperText="Typically starts with the thingId/telemetry/ followed by a unique path identifying the topic and other selectors e.g: 'ai.composiv.sandbox.f1tenth:mycar/telemetry/atopic/abcd-1234-efgh'."
-          >
-            <TextInput
-              isRequired
-              type="text"
-              id="simple-form-name-01"
-              name="simple-form-name-01"
-              aria-describedby="simple-form-name-01-helper"
-              value={target}
-              onChange={handleTargetChange}
-            />
-          </FormGroup>
-          <ActionGroup>
-            <Button variant="primary" onClick={_e => addTelemetery()}>Add Telemetry</Button>
-          </ActionGroup>
-        </Form>
-      </Card>}
+      <TelemetryForm client={client} definition={newDefinition} loading={loading} onSubmit={ (a, b, c) => {
+        setLoading(true)
+        doTelemetry(a, b, c)
+        setNewDefinition(undefined)
+      }} />
       <Card style={{ textAlign: 'left', margin: '10px' }} component="div" >
         <CardTitle
           style={{
@@ -301,11 +321,13 @@ const VehicleTelemetry = () => {
             onClear={(evt) => onFilterChange('', evt)}
           />
           <br />
-          <Accordion isBordered displaySize="default">
-            {!!telemetryList &&
-              filterList(telemetryList, 'topic', filterValue).map(
-                (telemetry, index) => {
-                  return (
+
+          { loading
+            ? <Spinner isSVG size="xl" aria-label="Getting telemetry" />
+            : <Accordion isBordered displaySize="default">
+            {!!telemetryList && filterList(telemetryList, 'topic', filterValue).map(
+              (telemetry, index) => {
+                return (
                     <AccordionItem key={index}>
                       <AccordionToggle
                         onClick={() => {
@@ -331,39 +353,39 @@ const VehicleTelemetry = () => {
                         </AccordionExpandedContentBody>
                       </AccordionContent>
                     </AccordionItem>
-                  )
-                }
-              )}
-          </Accordion>
+                )
+              }
+            ) }
+          </Accordion> }
         </CardBody>
       </Card>
       <Grid hasGutter>
-          <GridItem>
-            <Gallery
-              hasGutter
-              style={ galleryStyle }>
-              {!!telemetryData && Object.keys(telemetryData).map((k, i) => {
-                return <Card style={{ textAlign: 'left', margin: '10px' }} component="div" >
-                  <CardTitle
-                    style={{
-                      textAlign: 'center',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      fontWeight: 500,
-                      background: 'black',
-                      color: 'white'
-                    }}
-                  >{k}</CardTitle>
-                  <CardBody>
-                    <ReactJson name={false} src={telemetryData[k]} />
-                  </CardBody>
-                </Card>
-              })}
-            </Gallery>
-          </GridItem>
-          ;
-        </Grid>
+        <GridItem>
+          <Gallery
+            hasGutter
+            style={galleryStyle}>
+            {!!telemetryData && Object.keys(telemetryData).map((k, i) => {
+              return <Card style={{ textAlign: 'left', margin: '10px' }} component="div" >
+                <CardTitle
+                  style={{
+                    textAlign: 'center',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    fontWeight: 500,
+                    background: 'black',
+                    color: 'white'
+                  }}
+                >{k}</CardTitle>
+                <CardBody>
+                  <ReactJson name={false} src={telemetryData[k]} />
+                </CardBody>
+              </Card>
+            })}
+          </Gallery>
+        </GridItem>
+        ;
+      </Grid>
     </>
   )
 }
