1ec6d4d624abdb7792348de1531dbe0f8d84684c
Alp SARICA <alp.sarica@eteration.com>
2024-02-29 19:08:22 +0300

    updated vehicle pinging

--- src/common/mqtt.js
+++ src/common/mqtt.js
@@ -14,15 +14,16 @@
 //    Composiv.ai, Eteration A.S. - initial API and implementation
 //
 //
+import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 
-export function thingTopic (thing) {
+export function thingTopic(thing) {
   const correlation = uuidv4()
   const topic = `db-${thing}/agent/${correlation}`
   return { topic, correlation }
 }
 
-export function publishCommand (client, topic, target, payload) {
+export function publishCommand(client, topic, target, payload) {
   client.publish(
     topic,
     JSON.stringify({
@@ -37,11 +38,11 @@ export function publishCommand (client, topic, target, payload) {
     })
 }
 
-export function ping (client, thing, response) {
+export function ping(client, thing, response) {
   const correlationId = uuidv4()
   if (!client || !thing) return
   const ttopic = `ping-${thing}/agent/${correlationId}`
-  client.subscribe(ttopic, (_err) => {})
+  client.subscribe(ttopic, (_err) => { })
   client.on('message', (topic, payload, packet) => {
     if (topic === ttopic) {
       response(topic, payload, packet)
@@ -62,4 +63,61 @@ export function ping (client, thing, response) {
         correlationData: correlationId
       }
     })
+}
+
+export function ping2(client, thing, response) {
+  const thingSplitted = thing.split(':')
+  const thingNamespace = thingSplitted[0]
+  const thingId = thingSplitted[1]
+
+  const thingTopic = `muto/${thing}`
+  const correlationId = uuidv4()
+
+  const payload = {
+    topic: `${thingNamespace}/${thingId}/things/live/messages/agent/commands/ping`,
+    headers: {
+      'content-type': 'application/json',
+      'correlation-data': correlationId
+    },
+    path: '/inbox/messages/agent/commands/ping',
+    value: ''
+  }
+
+  if (!client || !thing) return
+
+  client.unsubscribe(thingTopic)
+  client.subscribe(thingTopic, (_err) => { })
+  client.on('message', (topic, payload, packet) => {
+    const payloadJSON = JSON.parse(payload)
+    if (
+      (thingTopic === topic) &&
+      (correlationId == payloadJSON.headers["correlation-data"]) &&
+      (payloadJSON.path.startsWith("/outbox"))
+    ) {
+      response(topic, payload, packet)
+      client.unsubscribe(thingTopic)
+    }
+  })
+  client.publish(thingTopic, JSON.stringify(payload))
+}
+
+
+export function ping3(client, thing, response) {
+  const thingSplitted = thing.split(':')
+  const thingNamespace = thingSplitted[0]
+  const thingId = thingSplitted[1]
+
+  const url = `https://sandbox.composiv.ai/api/2/things/${thingNamespace}:${thingId}/inbox/messages/agent/commands/ping`
+  const data = {}
+  const config = {
+    auth: {
+      username: 'ditto',
+      password: 'ditto'
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  axios.post(url, data, config)
 }
